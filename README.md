538f535a180b7a41b303f7adeb7c3e85e994f20f
DrkCM <139226423+DrkCM@users.noreply.github.com>
2023-08-25 20:42:27 +0100

    Upload the Challenge-5

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Challenge-5
 Module-5 Advance JS.  Challenge: Pasword Generator created using javascript.
+
+![Alt text](<Demo 01.png>)  ![Alt text](<Demo 02.png>)
---
+++ script.js
@@ -0,0 +1,225 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+  // variable to store length of password from user input.
+   let length = parseInt(
+    prompt('How many characters would you like your password to contain?')
+   )
+  
+   // show you a warning in case of you type letters instead of numbers
+   if(isNaN(length) === true){
+    alert('Password length must be provided as number');
+    return;
+   }
+
+  // show you a warning when password is less than 8 characteres
+   if(length < 8) {
+    alert('Password length must be at least 8 characteres');
+    return;
+   }
+
+   // show you a warning when password exceed 128 characteres
+   if(length > 128) {
+    alert('Password length must not exceed 128 characteres');
+    return;
+   }
+
+   // variable to include special characteres
+   let hasSpecialCharacters = confirm(
+    'Click OK to confirm including special characters'
+   )
+
+    // variable to include numeric characteres
+   let hasNumericCharacters = confirm(
+    'Click OK to confirm including numeric characters'
+   )
+
+    // variable to include lowercase characteres
+   let hasLowerCasedCharacters = confirm(
+    'Click OK to confirm including lowercase characters'
+   )
+
+    // variable to include uppercase characteres
+   let hasUpperCasedCharacters = confirm(
+    'Click OK to confirm including uppercase characters'
+   )
+   // show you a warning when you have no choose at least one option
+   if(hasLowerCasedCharacters === false &&
+      hasUpperCasedCharacters === false &&
+      hasSpecialCharacters === false &&
+      hasNumericCharacters === false) {
+       alert(`Must select at least one character type`);
+       return;
+      }
+
+// Variable password options to create the Password with all caracteristics
+ let passwordOptions = {
+  length: length,
+  hasSpecialCharacters: hasSpecialCharacters,
+  hasLowerCasedCharacters: hasLowerCasedCharacters,
+  hasUpperCasedCharacters: hasUpperCasedCharacters,
+  hasNumericCharacters: hasNumericCharacters
+ }
+
+console.log(passwordOptions);
+
+return passwordOptions;
+
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+   let randomIndex = Math.floor(Math.random() * arr.length)
+   let randomElement = arr[randomIndex];
+
+   return randomElement;
+}
+
+// Function to generate password with user input
+function generatePassword() {
+   let options = getPasswordOptions();
+   console.log(options);
+
+   let result = []
+
+   let possibleCharacter = []
+
+   let guaranteedCharacter = []
+
+   if(options.hasSpecialCharacters) {
+    possibleCharacter = possibleCharacter.concat(specialCharacters);
+    guaranteedCharacter.push(getRandom(specialCharacters))
+   }
+
+   if(options.hasLowerCasedCharacters) {
+    possibleCharacter = possibleCharacter.concat(lowerCasedCharacters);
+    guaranteedCharacter.push(getRandom(lowerCasedCharacters))
+   }
+
+   if(options.hasUpperCasedCharacters) {
+    possibleCharacter = possibleCharacter.concat(upperCasedCharacters);
+    guaranteedCharacter.push(getRandom(upperCasedCharacters))
+   }
+
+   if(options.hasNumericCharacters) {
+    possibleCharacter = possibleCharacter.concat(numericCharacters);
+    guaranteedCharacter.push(getRandom(numericCharacters))
+   }
+
+   console.log(possibleCharacter);
+   console.log(guaranteedCharacter);
+
+   for(let index = 0; index < options.length; index++) {
+    var generated = getRandom(possibleCharacter);
+    console.log(generated);
+    result.push(generated);
+   }
+
+   console.log(result);
+
+   return result.join(""); 
+
+}
+ 
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
